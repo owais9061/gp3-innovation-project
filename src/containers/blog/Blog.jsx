ba88b9e883860da4bee5e1319099f99e80c4cf5c
owais9061 <owaischaudhry906@gmail.com>
2023-01-13 17:28:59 +0500

    Added Styles & JSX files for everything

---
+++ src/containers/blog/Blog.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './blog.css';
+
+const blog = () => {
+  return (
+    <div>Blogs</div>
+  )
+}
+
+export default blog
---
+++ src/containers/features/Features.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './features.css';
+
+const features = () => {
+  return (
+    <div>Features</div>
+  )
+}
+
+export default features
